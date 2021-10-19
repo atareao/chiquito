6b89c7fbc25b96f19028adf4217d8106112cca41
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2021-10-19 20:44:02 +0200

    Actualización a Alpine Distro

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,35 @@
-FROM ubuntu
+FROM alpine:3.13
+
+ENV TZ=Europe/Madrid
 MAINTAINER Lorenzo Carbonell <a.k.a. atareao> "lorenzo.carbonell.cerezo@gmail.com"
-RUN apt-get update -y
-COPY ./requirements.txt /app/requirements.txt
+
+ENV PYTHONUNBUFFERED=1
+
+COPY ./requirements.txt /requirements.txt
+
+RUN echo "**** install Python ****" && \
+    apk add --update --no-cache python3 tini tzdata && \
+    if [ ! -e /usr/bin/python ]; then ln -sf python3 /usr/bin/python ; fi && \
+    \
+    echo "**** install pip ****" && \
+    python3 -m ensurepip && \
+    rm -r /usr/lib/python*/ensurepip && \
+    pip3 install --no-cache --upgrade pip setuptools wheel && \
+    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
+    echo "**** install dependencies **** " && \
+    pip3 install --no-cache-dir -r /requirements.txt && \
+    rm -rf /var/lib/apt/lists/* /requirements.txt && \
+    echo "**** create user ****" && \
+    addgroup dockeruser && \
+    adduser -h /app -G dockeruser -D dockeruser && \
+    mkdir -p /app/database && \
+    chown -R dockeruser:dockeruser /app
+
+
 WORKDIR /app
-RUN apt-get install -y $(grep -vE "^\s*#" requirements.txt  | tr "\n" " ")
-COPY src/* /app/
-ENTRYPOINT ["python3"]
-CMD ["app.py"]
+USER dockeruser
+COPY start.sh /start.sh
+COPY ./app /app
+
+ENTRYPOINT ["tini", "--"]
+CMD ["/bin/sh", "/start.sh"]
--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021 Lorenzo Carbonell <a.k.a. atareao>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import sqlite3
 import datetime
 import random
@@ -6,14 +28,23 @@ from flask import Flask, jsonify, make_response, abort, url_for
 
 app = Flask(__name__)
 
+
+@app.route('/status', methods=['GET'])
+def get_status():
+    return make_response(jsonify({'status': 'Up and running'}), 200)
+
+
 def create_joke(id, author, value, created_at, updated_at):
+    created_at = datetime.datetime.fromtimestamp(created_at).isoformat()
+    updated_at = datetime.datetime.fromtimestamp(updated_at).isoformat()
     return {'id': id,
             'author': author,
             'value': value,
             'url': url_for('get_joke', joke_id=id, _external=True),
-            'created_at': datetime.datetime.fromtimestamp(created_at).isoformat(),
-            'updated_at': datetime.datetime.fromtimestamp(updated_at).isoformat()}
- 
+            'created_at': created_at,
+            'updated_at': updated_at}
+
+
 def select(sqlquery, one=False):
     conn = sqlite3.connect('frases.db')
     cursor = conn.cursor()
@@ -21,7 +52,8 @@ def select(sqlquery, one=False):
     if one:
         return cursor.fetchone()
     return cursor.fetchall()
- 
+
+
 def select_joke(sqlquery, one=False):
     data = select(sqlquery, one)
     if one:
@@ -34,11 +66,13 @@ def select_joke(sqlquery, one=False):
             joke = create_joke(i[0], i[1], i[2], i[3], i[4])
             jokes.append(joke)
         return jokes
- 
+
+
 @app.route('/api/1.0/jokes', methods=['GET'])
 def get_jokes():
     sqlquery = 'SELECT * FROM JOKES'
     return jsonify(select_joke(sqlquery)), 201
+
 
 @app.route('/api/1.0/jokes/<int:joke_id>', methods=['GET'])
 def get_joke(joke_id):
@@ -48,6 +82,7 @@ def get_joke(joke_id):
         return jsonify(result), 201
     abort(404)
 
+
 @app.route('/api/1.0/jokes/random', methods=['GET'])
 def get_random():
     sqlquery = 'SELECT COUNT(1) FROM JOKES'
@@ -56,10 +91,12 @@ def get_random():
     id = random.randint(1, result[0])
     return get_joke(id)
 
-    
+
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    msg = str(error)
+    return make_response(jsonify({'status': 'error', 'msg': msg}), 404)
+
 
 def get():
     conn = sqlite3.connect('../frases.db')
